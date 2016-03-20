a88542272fe4b8f8cdd02243579a0c581758ed84
Stephen Cartwright <stvcartwright@aim.com>
2016-03-20 19:40:22 -0400

    Initial Commit

---
+++ obliterate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  obliterate
+//
+//  Created by Stephen Cartwright on 3/20/16.
+//  Copyright © 2016 Ōmagatoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
